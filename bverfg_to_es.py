f838164b5360085b7937f307d731920ed4807d21
Tilo Himmelsbach <dertilo@gmail.com>
2020-05-19 19:16:30 +0200

    bverfg_to_es.py

---
+++ bverfg_to_es.py
@@ -0,0 +1,29 @@
+from time import sleep, time
+
+from es_parallel_bulk import populate_es_parallel_bulk
+from esutil.es_util import build_es_client
+
+if __name__ == "__main__":
+    INDEX_NAME = "bverfg"
+    TYPE = "decision"
+    es = build_es_client()
+
+    es.indices.delete(index=INDEX_NAME, ignore=[400, 404])
+    es.indices.create(index=INDEX_NAME, ignore=400)
+    sleep(3)
+
+    files = ["BverfG.jsonl.gz"]
+
+    start = time()
+    populate_es_parallel_bulk(es, files, INDEX_NAME, TYPE)
+    dur = time() - start
+
+    sleep(3)
+    count = es.count(
+        index=INDEX_NAME, doc_type=TYPE, body={"query": {"match_all": {}}}
+    )["count"]
+    print("populating es-index with %d documents took: %0.2f seconds" % (count, dur))
+
+'''
+populating es-index with 16197 documents took: 10.25 seconds
+'''
